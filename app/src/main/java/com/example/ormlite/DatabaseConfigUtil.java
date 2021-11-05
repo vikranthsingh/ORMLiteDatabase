13f5219e48d1fba8f62114ee138d63642822070d
Vikranth <vikranth.civilenginer@gmail.com>
2021-11-05 17:51:41 +0530

    Ormlite

---
+++ app/src/main/java/com/example/ormlite/DatabaseConfigUtil.java
@@ -0,0 +1,13 @@
+package com.example.ormlite;
+
+import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class DatabaseConfigUtil extends OrmLiteConfigUtil {
+    private static final Class<?>[] classs = new Class[]{Notes.class};
+    public static void main(String[] args) throws IOException, SQLException {
+        writeConfigFile("ormlite_config.txt", classs);
+    }
+}
