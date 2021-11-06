6371e0478285ee5a96f1c47875fbe8f341e6513e
Vikranth <vikranth.civilenginer@gmail.com>
2021-11-06 10:15:09 +0530

    Ormlite

--- app/src/main/java/com/example/ormlite/DatabaseConfigUtil.java
+++ app/src/main/java/com/example/ormlite/DatabaseConfigUtil.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class DatabaseConfigUtil extends OrmLiteConfigUtil {
-    private static final Class<?>[] classs = new Class[]{Notes.class};
+    private static final Class<?>[] classes = new Class[]{Notes.class};
     public static void main(String[] args) throws IOException, SQLException {
-        writeConfigFile("ormlite_config.txt", classs);
+        writeConfigFile("ormlite_config.txt", classes);
     }
 }
