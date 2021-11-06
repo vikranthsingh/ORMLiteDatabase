7f42469212b57f7683d701f5a2e542f5c801f5b4
Vikranth <vikranth.civilenginer@gmail.com>
2021-11-06 11:40:18 +0530

    Ormlite

--- app/src/main/java/com/example/ormlite/MainActivity.java
+++ app/src/main/java/com/example/ormlite/MainActivity.java
@@ -6,19 +6,20 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    DatabaseHelper dbHelper;
+public class MainActivity extends OrmLiteBaseActivity<DatabaseHelper> {
+    //DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper = OpenHelperManager.getHelper(this, DatabaseHelper.class);
-        RuntimeExceptionDao<Notes, Integer> runtimeExceptionDao = dbHelper.getNoteRuntimeExceptionDao();
+        //dbHelper = OpenHelperManager.getHelper(this, DatabaseHelper.class);
+        RuntimeExceptionDao<Notes, Integer> runtimeExceptionDao = getHelper().getNoteRuntimeExceptionDao();
         runtimeExceptionDao.createOrUpdate(new Notes("Note 1", "Note 1 Text"));
         runtimeExceptionDao.createOrUpdate(new Notes("Note 2", "Note 2 Text"));
 
@@ -28,6 +29,6 @@ public class MainActivity extends AppCompatActivity {
         List<Notes> note1 = runtimeExceptionDao.queryForEq("id", 1);
         Log.d("demo1", note1.toString());
 
-        OpenHelperManager.releaseHelper();
+        //OpenHelperManager.releaseHelper();
     }
 }
